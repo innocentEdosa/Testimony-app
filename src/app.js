afa076fc059f964574c31519046236ce0ec14243
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2019-08-18 12:20:40 +0100

    166980943(chore): Setup node project [Delivers 166980943]

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-import express from express;
+import express from 'express';
+
+const app = express();
+app.use('/testimony', (req, res) => {
+  res.send('what a beautiful name it is');
+});
+app.listen(process.env.PORT || 9999);
